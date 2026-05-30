3db7fe2dd46002da3baa0ca7369cef8202c69d4d
Alonya <96838192+AlonyaJS@users.noreply.github.com>
2026-05-30 04:23:05 +0200

    Update main.ts

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,42 +1,7 @@
-{
-  const resourceName = GetCurrentResourceName();
+on('onResourceStart', (resourceName: string) => {
+	if (resourceName !== GetCurrentResourceName()) {
+		return;
+	}
 
-  on('onClientResourceStart', (startedResourceName: string) => {
-    if (startedResourceName !== resourceName) {
-      return;
-    }
-
-    console.log(`[${resourceName}] Client started`);
-  });
-
-  RegisterCommand(
-    'showtestpanel',
-    () => {
-      const message: NuiMessage = {
-        action: 'show',
-        title: 'Test Panel',
-        text: 'Hello from the FiveM client.'
-      };
-
-      SendNUIMessage(message);
-      SetNuiFocus(true, true);
-    },
-    false
-  );
-
-  RegisterNuiCallback('testButton', (_data: unknown, cb: (response: { ok: boolean }) => void) => {
-    console.log(`[${resourceName}] NUI button clicked`);
-
-    cb({ ok: true });
-  });
-
-  RegisterNuiCallback('hidePanel', (_data: unknown, cb: (response: { ok: boolean }) => void) => {
-    const message: NuiMessage = {
-      action: 'hide'
-    };
-
-    SendNUIMessage(message);
-    SetNuiFocus(false, false);
-    cb({ ok: true });
-  });
-}
+	console.log(`[${resourceName}] Server started`);
+});
