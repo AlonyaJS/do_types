e130a88a61a276514f1d692fc2dcfb8c1851bb52
Alonya <96838192+AlonyaJS@users.noreply.github.com>
2026-05-30 04:03:56 +0200

    Update main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,1 @@
-{
-  const resourceName = GetCurrentResourceName();
 
-  on('onResourceStart', (startedResourceName: string) => {
-    if (startedResourceName !== resourceName) {
-      return;
-    }
-
-    console.log(`[${resourceName}] Server started`);
-  });
-}
